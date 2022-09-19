e79597819cc791142e43095d5cdad4e44cda959a
dioncahya17 <dioncahya170292@gmail.com>
2022-09-19 23:40:49 +0700

    coba push

--- promise.js
+++ promise.js
@@ -1,7 +1,28 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+let hasilMarah = 0
+let hasilTidakMarah = 0
+
+const promiseOutput = emosi => {
+  if (emosi === 'marah') {
+    //me-return suatu fungsi
+    return promiseTheaterIXX().then((value) => {
+      value.forEach((array) => {
+        if (array.hasil === 'marah') {
+          hasilMarah++;
+        }
+        else if (array.hasil === 'tidak marah') {
+          hasilTidakMarah++;
+        }
+        else {
+          console.log('Error')
+        }
+      })
+    })
+  }
+}
+
 
 module.exports = {
   promiseOutput,
