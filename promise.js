02e33e59bd85097c7fd35ccaca197fc020ec1d65
dioncahya17 <dioncahya170292@gmail.com>
2022-09-22 14:23:54 +0700

    challenge-promises, OTW

--- promise.js
+++ promise.js
@@ -1,8 +1,12 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = emosi => {
+  // getData().then(data => console.log(data));
+  promiseTheaterIXX().then(data => console.log(data))
+}
+
 
 module.exports = {
-  promiseOutput,
+  promiseOutput
 };
