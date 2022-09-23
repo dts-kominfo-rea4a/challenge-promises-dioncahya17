19be7d3466c0e8450f71bbbb61ad76512fb7e791
dioncahya17 <dioncahya170292@gmail.com>
2022-09-23 15:50:56 +0700

    3rd try - push challenge-promise

--- promise.js
+++ promise.js
@@ -2,9 +2,31 @@ const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
 const promiseOutput = emosi => {
-  promiseTheaterIXX().then((data => console.log(data)))
+  let hasilMarah = 0
+  let hasilTidakMarah = 0
+    if (emosi === 'marah') {
+      promiseTheaterIXX().then((data) => {
+        data.forEach ((array) => {
+        if (array.hasil === 'marah') {
+          hasilMarah++
+        } else if (array.hasil === 'tidak marah') {
+          hasilTidakMarah++
+        }  
+        })
+      })
+      promiseTheaterVGC().then((data2) => {
+        data2.forEach ((array) => {
+          if (array.hasil === 'marah') {
+            hasilMarah++
+          } else if (array.hasil === 'tidak marah') {
+            hasilTidakMarah++
+          }
+        })
+        console.log(hasilTidakMarah)
+        console.log(hasilMarah)
+      })
+    }
 }
-
 
 module.exports = {
   promiseOutput
